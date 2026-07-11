391ee9d60a16f8a44d5c9ed2975194c806b29930
Zly-u <zlyswork@gmail.com>
2026-07-12 02:50:43 +0300

    Implemented Doppler Extended

---
+++ Source/SoundClassMixer/Nodes/SoundNode_DopplerEx.cpp
@@ -0,0 +1,82 @@
+﻿#include "SoundNode_DopplerEx.h"
+#include "ActiveSound.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+/*-----------------------------------------------------------------------------
+         USoundNodeDopplerEx implementation.
+-----------------------------------------------------------------------------*/
+USoundNodeDopplerEx::USoundNodeDopplerEx(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	DopplerIntensity = 1.0f;
+	bUseSmoothing = false;
+	SmoothingInterpSpeed = 5.0f;
+}
+
+
+void USoundNodeDopplerEx::ParseNodes( FAudioDevice* AudioDevice, const UPTRINT NodeWaveInstanceHash, FActiveSound& ActiveSound, const FSoundParseParameters& ParseParams, TArray<FWaveInstance*>& WaveInstances )
+{
+	RETRIEVE_SOUNDNODE_PAYLOAD(sizeof(float));
+	DECLARE_SOUNDNODE_ELEMENT(float, CurrentPitchScale);
+
+	FSoundParseParameters UpdatedParams = ParseParams;
+
+	check(AudioDevice);
+	const TArray<FListener>& Listeners = AudioDevice->GetListeners();
+
+	int32 ListenerIndex = ActiveSound.GetClosestListenerIndex();
+	if (ListenerIndex >= Listeners.Num())
+	{
+		ListenerIndex = 0;
+	}
+
+	const FListener& Listener = Listeners[ListenerIndex];
+
+	// Default the parse to using the setting for smoothing
+	if (*RequiresInitialization)
+	{
+		*RequiresInitialization = 0;
+
+		// First time, do no smoothing, but initialize the current pitch scale value to the first value returned from this function
+		CurrentPitchScale = GetDopplerPitchMultiplier(CurrentPitchScale, false, Listener, ParseParams.Transform.GetTranslation(), ParseParams.Velocity, AudioDevice->GetDeviceDeltaTime());
+		UpdatedParams.Pitch *= CurrentPitchScale;
+	}
+	else
+	{
+		// Subsequent calls to this will do smoothing from the first initial value
+		UpdatedParams.Pitch *= GetDopplerPitchMultiplier(CurrentPitchScale, bUseSmoothing, Listener, ParseParams.Transform.GetTranslation(), ParseParams.Velocity, AudioDevice->GetDeviceDeltaTime());
+	}
+
+	Super::ParseNodes(AudioDevice, NodeWaveInstanceHash, ActiveSound, UpdatedParams, WaveInstances);
+}
+
+
+float USoundNodeDopplerEx::GetDopplerPitchMultiplier(float& CurrentPitchScale, bool bSmooth, FListener const& InListener, const FVector Location, const FVector Velocity, float DeltaTime)
+{
+	static constexpr float SpeedOfSoundInAirAtSeaLevel = 33000.f;		// cm/sec
+
+	FVector DistanceBetweenListenerAndSource = InListener.Transform.GetTranslation() - Location;
+	DistanceBetweenListenerAndSource = DistanceBetweenListenerAndSource.GetClampedToSize(0, MaxVelocityLimit);
+	
+	FVector const SourceToListenerNorm = DistanceBetweenListenerAndSource.GetSafeNormal();
+
+	// find source and listener speeds along the line between them
+	float const SourceVelMagTowardListener = Velocity | SourceToListenerNorm;
+	float const ListenerVelMagAwayFromSource = InListener.Velocity | SourceToListenerNorm;
+	
+	float const ListenerToSourceClampedDist = FMath::Clamp(SourceVelMagTowardListener - ListenerVelMagAwayFromSource, -MaxVelocityLimit, MaxVelocityLimit);
+	
+	float const InvDopplerPitchScale = 1.f - ( ListenerToSourceClampedDist / SpeedOfSoundInAirAtSeaLevel );
+	float const PitchScale = 1.f / InvDopplerPitchScale;
+	float const FinalPitchScale = FMath::Clamp((PitchScale - 1.f) * DopplerIntensity, -MaxPitchChangeLimit, MaxPitchChangeLimit) + 1.f;
+
+	if (bSmooth)
+	{
+		CurrentPitchScale = UKismetMathLibrary::FInterpTo(CurrentPitchScale, FinalPitchScale, DeltaTime, SmoothingInterpSpeed);
+
+		return CurrentPitchScale;
+	}
+	
+	return FinalPitchScale;
+}
---
+++ Source/SoundClassMixer/Nodes/SoundNode_DopplerEx.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "AudioDevice.h"
+#include "Sound/SoundNode.h"
+#include "SoundNode_DopplerEx.generated.h"
+
+struct FActiveSound;
+struct FSoundParseParameters;
+
+/** 
+ * Computes doppler pitch shift
+ */
+UCLASS(HideCategories = Object, EditInlineNew, Meta = (DisplayName = "Doppler Extended"))
+class USoundNodeDopplerEx : public USoundNode
+{
+	GENERATED_UCLASS_BODY()
+
+	/* How much to scale the doppler shift (1.0 is normal). */
+	UPROPERTY(EditAnywhere, Category = "Doppler Extended")
+	float DopplerIntensity;
+
+	/** Whether or not to do a smooth interp to our doppler */
+	UPROPERTY(EditAnywhere, Category = "Doppler Extended")
+	bool bUseSmoothing;
+
+	/** Speed at which to interp pitch scale */
+	UPROPERTY(EditAnywhere, Category = "Doppler Extended", meta = (EditCondition = "bUseSmoothing"))
+	float SmoothingInterpSpeed;
+	
+	
+	/** Speed past which the pitch will reset. */
+	UPROPERTY(EditAnywhere, Category = "Doppler Extended")
+	float MaxVelocityLimit = 99999.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Doppler Extended")
+	float MaxPitchChangeLimit = 10.f;
+
+	
+	public:
+		//~ Begin USoundNode Interface. 
+		virtual void ParseNodes( FAudioDevice* AudioDevice, const UPTRINT NodeWaveInstanceHash, FActiveSound& ActiveSound, const FSoundParseParameters& ParseParams, TArray<FWaveInstance*>& WaveInstances ) override;
+		//~ End USoundNode Interface. 
+
+		
+	protected:
+		float GetDopplerPitchMultiplier(float& CurrentPitchScale, bool bSmooth, FListener const& InListener, const FVector Location, const FVector Velocity, float DeltaTime);
+};
